265c89b30530b85f22f574420104a0b27fab88ba
Ganeshram <nagarathinam.ganeshram@gmail.com>
2019-09-02 18:58:40 +0530

    added new files

--- First.go
+++ First.go
@@ -1,8 +1,28 @@
 package main
 
 import "fmt"
+// import "math"
 
 func main() {
 
-	fmt.Printf("Hello world");
+	//fmt.Printf("Hello world");
+
+	//const pi = math.Pi
+	//const pi = 3.14
+
+	//fmt.Println(pi)
+
+	var name string = "Ganesh";
+	var nameaddress *string;
+	nameaddress = &name
+	var sss **string=&nameaddress;
+
+	fmt.Println(*nameaddress);
+	fmt.Println("*********************");
+	fmt.Println(nameaddress)
+	fmt.Println(sss)
+
+	
+
+
 }
